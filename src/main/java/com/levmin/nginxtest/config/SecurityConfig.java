d001487ed16165f1146fce6f5c955d4fa8b92fd7
zy5201ljf@gmail.com <zy5201ljf@gmail.com>
2026-01-23 22:25:51 +0800

    代码格式化

--- src/main/java/com/levmin/nginxtest/config/SecurityConfig.java
+++ src/main/java/com/levmin/nginxtest/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.levmin.nginxtest.config;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Encoders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,8 +14,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import javax.crypto.SecretKey;
 
 @Configuration
 @EnableWebSecurity
